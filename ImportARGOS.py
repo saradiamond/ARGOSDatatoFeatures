d4942c91a8b623e3143577c41751cd3fa30b6fc2
saradiamond <sara.diamond@duke.edu>
2022-10-24 15:28:36 -0400

    adding a spatial reference parameter to FC

--- ImportARGOS.py
+++ ImportARGOS.py
@@ -18,12 +18,13 @@ arcpy.env.overwriteOutput = True
 #%% Import a single file as a file object
 # Set input variables (Hard-wired)
 inputFile = 'V:/ARGOSTracking/ARGOSTracking/data/ARGOSData/1997dg.txt'
+outputSR = arcpy.SpatialReference(54002)
 outputFC = "V:/ARGOSTracking/ARGOSTracking/scratch/ARGOStrack.shp"
 
 ## Prepare a new feature class to which we'll add tracking points
 # Create an empty feature class; requires the path and name as separate paramters
 outPath,outName = os.path.split(outputFC)
-arcpy.CreateFeatureclass_management(outPath, outName)
+arcpy.CreateFeatureclass_management(outPath, outName, "POINT",'','','',outputSR)
 
 #%% Construct a while loop to iterate through all lines in the datafile
 # Open the ARGOS data file for reading
